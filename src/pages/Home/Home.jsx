8da5440a5175cf0a43ea12162714d51fbc6743c1
Mersad <mersadmh007.1380@gmail.com>
2024-09-07 12:13:23 +0330

    Add new changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
   let [value, setValue] = useState([]);
-  const maxItems = 5; // Maximum number of items allowed
+  const maxItems = 4; // Maximum number of items allowed
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -77,6 +77,49 @@ export default function Home() {
         onSubmit={onSubmitHandler}
       >
         <form action="" dir="rtl">
+          <div id="registrationForm" className="selectbox-wrapper mb-4">
+            <select>
+              <option value="-1">-- انتخاب کنید --</option>
+              <option value="زکات">زکات</option>
+              <option value="زکات فطره">زکات فطره</option>
+              <option value="کفارات">کفارات</option>
+              <option value="صدقه">خمس</option>
+              <option value="صدقه">صدقه</option>
+              <option value="رد مظالم">رد مظالم</option>
+              <option value="تبرعات و امور خیریه">تبرعات و امور خیریه</option>
+            </select>
+            <div onClick={() => removeHandler(data.id)}>
+              {/* <svg
+                className="close-button"
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                viewBox="0 0 320 512"
+              >
+                <path d="M317.7 402.3c3.125 3.125 3.125 8.188 0 11.31c-3.127 3.127-8.186 3.127-11.31 0L160 267.3l-146.3 146.3c-3.127 3.127-8.186 3.127-11.31 0c-3.125-3.125-3.125-8.188 0-11.31L148.7 256L2.344 109.7c-3.125-3.125-3.125-8.188 0-11.31s8.188-3.125 11.31 0L160 244.7l146.3-146.3c3.125-3.125 8.188-3.125 11.31 0s3.125 8.188 0 11.31L171.3 256L317.7 402.3z"></path>
+              </svg> */}
+            </div>
+            <div className="d-flex align-items-center">
+              <label className="gender-button" htmlFor="male">
+                عام
+              </label>
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                className="d-none female"
+              />
+              <label className="gender-button" htmlFor="male">
+                سادات
+              </label>
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                className="d-none male"
+              />
+            </div>
+            <input className="input" type="number" placeholder="مبلغ" />
+          </div>
           {value.map((data, index) => (
             <div
               key={data.id}
